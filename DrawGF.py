b8d2e12d5f5f4e3a093ac33cac05d3c6efa0ed55
Ja <peng.cheng@my.jcu.edu.au>
2020-04-21 19:20:57 +0800

    Draw GF

---
+++ DrawGF.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageFilter, ImageOps
+image = Image.open('E:\\picture\\1.png')
+def dodge(a, b, alpha):
+    img1 = img.convert('L')
+    img2 = img1.copy()
+    img2 = ImageOps.invert(img2)
+    for i in range(blur): #mo hu du
+        imag2 = img2.filter(ImageFilter.BLUR)
+    width, height = img1.size
+    for x in range (width):
+        for y in range(height):
+            a = img1.getpixel((x, y))
+            b = img2.getpixel((x, y))
+            img1.putpixel((x ,y)), dodge(a, b alpha)
+    img1.show()
+    img1.save('E:\\picture\\1.png')
+
+draw(img)
